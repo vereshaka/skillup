f5ec6116c495a63d1332566436f517c5cf053ab2
yevgenyv <yevgeny.vasilyev@sytoss.com>
2019-10-24 10:29:57 +0300

    Cucumber reporting added

--- itest/features/shared_objects/common-objects.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    getValue(resultItems, check, resultItemsIndex, element) {
-        resultItems[resultItemsIndex].findElement(element).then(element => {
-            element.getText().then(value => expect(check).to.be.equal(value));
-        });
-    }
-};
