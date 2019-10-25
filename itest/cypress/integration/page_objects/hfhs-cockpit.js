1a8546354207bd361b2a6e51a0d75edbf02e2225
Mikhail Bugaevsky <mikhail.bugaevsky@sytoss.com>
2019-10-25 11:46:43 +0300

    [CCF-828] Replaced finding by caption for finding by id of element

--- itest/cypress/integration/page_objects/hfhs-cockpit.js
+++ itest/cypress/integration/page_objects/hfhs-cockpit.js
@@ -1,13 +1,19 @@
 
 module.exports = {
+  elements: {
+    product_move: 'openPM',
+  },
+  getElementIdByName(elementName) {
+    return module.exports.elements[elementName];
+  },
   checkUserCredentials(username, keycloakName, table) {
   },
   checkBusinessTransactionWidgetExistence() {
     cy.get('div.BusinessTransactionsWrapper');
   },
   openWidget(widgetName) {
-    cy.get('button')
-      .contains(widgetName)
+    cy
+      .get(`button[id=${module.exports.getElementIdByName(widgetName)}]`)
       .click();
   },
 };
