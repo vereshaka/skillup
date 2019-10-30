1fbd0429d08696fa6cc2211a64cb1ce96e0a3c91
Mikhail Bugaevsky <mikhail.bugaevsky@sytoss.com>
2019-10-30 12:42:04 +0200

    [CCF-830] Refactored 'portal-utils' file

--- itest/cypress/integration/page_objects/utils/portal-utils.js
+++ itest/cypress/integration/page_objects/utils/portal-utils.js
@@ -6,30 +6,43 @@ import {
 
 export const openWidget = (widgetName) => {
   cy.wait(wait.mediumWait);
-  cy.get('body').then(($body) => {
-    if ($body.find(`span:contains(${widgetName})`).length) {
-      cy.get(`button[id=${elements[widgetName]}]`)
-        .click();
-    } if (widgetName === 'Search Product') {
+  cy
+    .get('body')
+    .then(($body) => {
+      if ($body.find(`span:contains(${widgetName})`).length) {
+        cy
+          .get(`button[id=${elements[widgetName]}]`)
+          .click();
+      } if (widgetName === 'Search Product') {
       // TODO: ivanp: Should be refactored when CCF-840  will be done
-      cy.get('span[class="Icon faPlusSquare fa2x AddProduct "]').click();
-    }
-  });
+        cy
+          .get('span[class="Icon faPlusSquare fa2x AddProduct "]')
+          .click();
+      }
+    });
 };
 
 export const openLoginForm = () => {
   cy.visit(getPortalURL());
   cy.wait(wait.shortWait);
-  cy.get('body').then(($body) => {
-    if ($body.find('div.logout').length) {
-      cy.get('a[href="/portal/_/api/logout"]').click();
-    }
-  });
+  cy
+    .get('body')
+    .then(($body) => {
+      if ($body.find('div.logout').length) {
+        cy
+          .get('a[href="/portal/_/api/logout"]')
+          .click();
+      }
+    });
 };
 
 export const openCockpit = (cockpitName) => {
-  cy.get('div.menu-drawer').click();
-  cy.get(`a:contains(${cockpitName})`).click();
+  cy
+    .get('div.menu-drawer')
+    .click();
+  cy
+    .get(`a:contains(${cockpitName})`)
+    .click();
   cy.wait(wait.shortWait);
 };
 
