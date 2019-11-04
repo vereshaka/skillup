bfa9a981b804e9da5f61f97306926ea72fbcf300
Ivan Pivovarov <ivan.pivovarov@extern.a1.at>
2019-11-04 10:41:29 +0200

    [CCF-829]

--- itest/cypress/support/page_objects/product-move-widget.js
+++ itest/cypress/support/page_objects/product-move-widget.js
@@ -34,7 +34,7 @@ class ProductMoveWidget extends AbstractWidget {
     new SearchProductWidget().searchAndAddAll(query);
   };
 
-  specifyAccount = (account, query:string, group) => {
+  specifyAccount = (account, query:string, group:string) => {
     new AddAccountWidget().addAccount(account, query, group);
   };
 
