efaf6a8a67e0012872c1cac6cff7c47275f7c14b
Ivan Pivovarov <ivan.pivovarov@extern.a1.at>
2019-11-22 16:36:08 +0200

    [CCF-864] Add possibility to work with different environment.

--- itest/cypress/support/page_objects/utils/config.js
+++ itest/cypress/support/page_objects/utils/config.js
@@ -1,2 +1,14 @@
 export const getPassword = (username) => username;
-export const getPortalURL = () => 'https://gucci-portal.k8s.sytoss.intra';
+export const getPortalURL = () => {
+  const environment = Cypress.env('environment');
+  switch (environment) {
+    case 'local':
+      return 'https://gucci-portal.k8s.sytoss.intra';
+    case 'int':
+      return '';
+    case 'dev':
+      return '';
+    default:
+      throw new Error(`Unsupported environment. Name: ${environment}`);
+  }
+};
