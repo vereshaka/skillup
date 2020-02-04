07828b916350df1cd0cb4d8759f793eee3bb3860
Ivan Pivovarov <ivan.pivovarov@extern.a1.at>
2020-02-04 10:50:06 +0200

    [CCF-1090] Change selectors.

--- itest/webpack.config.js
+++ itest/webpack.config.js
@@ -1,7 +1,12 @@
+const path = require('path');
 
 module.exports = {
   resolve: {
     extensions: ['.ts', '.js'],
+    modules: [
+      path.resolve(__dirname, './cypress'),
+      path.resolve(__dirname, './node_modules'),
+    ],
   },
   node: { fs: 'empty', child_process: 'empty', readline: 'empty' },
   module: {
