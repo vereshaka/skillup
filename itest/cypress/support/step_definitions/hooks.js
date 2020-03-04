664462baa11b09394b5bfe0344588f6544707331
Ivan Pivovarov <ivan.pivovarov@extern.a1.at>
2020-03-04 17:08:13 +0200

    [CCF-xxX] Add mock mod selection before tests.

--- itest/cypress/support/step_definitions/hooks.js
+++ itest/cypress/support/step_definitions/hooks.js
@@ -9,6 +9,7 @@ const gucciWorld = new GucciWorld();
 
 Before(() => {
   // TODO: do nothing
+  cy.request('PUT', 'http://mock.k8s.sytoss.intra/api/configure', { mode: 'read_personal_only', timeout: 2000 });
 });
 
 After(() => {
