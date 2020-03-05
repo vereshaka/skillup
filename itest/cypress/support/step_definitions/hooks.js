1d3f64795a1554e72ce90189b9dc1c63dce2160e
Ivan Pivovarov <ivan.pivovarov@extern.a1.at>
2020-03-05 10:31:22 +0200

    [CCF-xxX] Add mock mod selection before tests.

--- itest/cypress/support/step_definitions/hooks.js
+++ itest/cypress/support/step_definitions/hooks.js
@@ -8,15 +8,8 @@ import GucciWorld from '../page_objects/common/gucci-world';
 const gucciWorld = new GucciWorld();
 
 Before(() => {
-  switch (Cypress.env('configFile')) {
-    case 'local':
-      cy.request('PUT', 'http://mock.k8s.sytoss.intra/api/configure', { mode: 'read_personal_only', timeout: 2000 });
-      break;
-    case 'hfhsmock':
-      cy.request('PUT', 'https://hfhs-sm.gucci-qa.at.inside/api/configure', { mode: 'read_personal_only', timeout: 2000 });
-      break;
-    default:
-      break;
+  if (Cypress.env('configFile') === 'hfhsmock') {
+    cy.request('PUT', `${Cypress.env('mockUrl')}`, { mode: 'read_personal_only', timeout: 2000 });
   }
 });
 
